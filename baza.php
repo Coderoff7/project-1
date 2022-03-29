8ce487ccb47960272869c7be682081bad620fc53
Coderoff7 <102596812+Coderoff7@users.noreply.github.com>
2022-03-29 15:34:32 +0300

    add

---
+++ baza.php
@@ -0,0 +1,39 @@
+<?php
+        $featured = [
+            [
+                'h1'=>"We're living some strange times",
+                'about'=>"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
+                'h2'=>"Dreaming of Las Vegas Crazyness",
+                'h3'=>"The beauty of this world is in your heart",
+                'h4'=>"San Francisco at its best view in all seasons"
+            ]
+        ];
+        $stories = [
+            [
+                'h1'=>"Autumn doesn't have to be nostalgic, you know?",
+                'h2'=>"Little red dress and a perfect summer",
+                'h3'=>"10 Things you should know about choosing your house",
+                'h4'=>"Best galleries in the world with photos",
+                'h5'=>"Thinking outside the box can help you prosper",
+                'h6'=>"Visiting the world means learning cultures",
+                'about'=>"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+            ]
+        ];
+        $post = [
+            [
+                'h1'=>"18 Things You Should Learn Before Moving Into a New Home",
+                'about1'=>"Holy grail funding non-disclosure agreement advisor ramen bootstrapping ecosystem. Beta crowdfunding iteration assets business plan paradigm shift stealth mass market seed money rockstar niche market marketing buzz market.",
+                'about2'=>"Burn rate release facebook termsheet equity technology. Interaction design rockstar network effects handshake creative startup direct mailing. Technology influencer direct mailing deployment return on investment seed round.",
+                'about3'=>"Termsheet business model canvas user experience churn rate low hanging fruit backing iteration buyer seed money. Virality release launch party channels validation learning curve paradigm shift hypotheses conversion. Stealth leverage freemium venture startup business-to-business accelerator market.",
+                'about4'=>"Gen-z strategy long tail churn rate seed money channels user experience incubator startup partner network low hanging fruit direct mailing. Client backing success startup assets responsive web design burn rate A/B testing metrics first mover advantage conversion.",
+                'about5'=>"Freemium non-disclosure agreement lean startup bootstrapping holy grail ramen MVP iteration accelerator. Strategy market ramen leverage paradigm shift seed round entrepreneur crowdfunding social proof angel investor partner network virality."
+            ]
+        ];
+        $author = [
+            [
+                'h1'=>"Life is worth living forever and ever",
+                'h2'=>"Best European capitals to visit and the costs implied",
+                'h3'=>"10 Things you should learn before visiting",
+                'about'=>"This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer."
+            ]
+        ];
